eadb187af2b96408f549d763b72b165b4892bf5f
Katie Wallshein <kwallshein@ufl.edu>
2022-03-27 12:30:32 -0400

    changes made after re-watching class video

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 var form = document.querySelector('form')
-var nameInput = document.querySelector('input[type=text]')
-var weatherInput = document.querySelector('input[type=weather]')
+var locationInput = document.querySelector('input[type=text]')
+var weatherInput = document.querySelector('input[type=number]')
 
-form.onsubmit = function(e){
-    e.preventdefault()
-    var formData = new FormData(e.target)
-    console.log(nameInput.value)
-    //*console.log(weatherInput.value)
-    //*console.log(Object.fromEntries(formData))
-    if(!usersInput) return
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=&appid=a502e2667bdfdef9e5cc8724f2b91a62&units=imperial') 
-    .then(function(res) {
-    return res.json() 
-})
-    .then(function(res) {
-    console.log(res) 
-    InputEvent.value = "";
-  })
-  .catch(function(error) {
-      console.log(error);
-      document.getElementById('weather').innerHTML = "Location not found."
-  })
-}
+    form.onsubmit = function(e){
+        e.preventDefault()
+        console.log(locationInput.value)
+        //*console.log(weatherInput.value)
+        //*console.log(Object.fromEntries(formData))
+        fetch('https://api.openweathermap.org/data/2.5/weather?appid=a502e2667bdfdef9e5cc8724f2b91a62&units=imperial&q=' + locationInput.value) 
+        .then(function(response) {
+        return response.json() 
+    })
+        .then(function(data) {
+        console.log(data) 
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    }
 
 
 //*fetch('https://api.openweathermap.org/data/2.5/weather?q=&appid=a502e2667bdfdef9e5cc8724f2b91a62&units=imperial') // make the request
